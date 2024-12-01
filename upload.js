587ba5ba67a28836ace318fe7269a9769fbe1e9f
Asbjørn Enge <asbjorn@hanafjedle.net>
2024-12-01 11:59:21 +0100

    Also dirs of any size

--- upload.js
+++ upload.js
@@ -47,35 +47,31 @@ export async function upload({ Tezos, filePath }) {
     await sendBatches(Tezos, tezbatches)
 
     console.log('File upload completed successfully.');
-  } 
-//  else if (stats.isDirectory()) {
-//    const files = getAllFilesSync(filePath)
-//    const oversizedFile = files.find(file => fs.statSync(file).size > CONFIG.network.BATCH_SIZE_LIMIT)
-//    if (oversizedFile) {
-//      console.error(`Error: File ${oversizedFile} exceeds the limit of ${CONFIG.network.BATCH_SIZE_LIMIT} bytes.`);
-//      process.exit(1);
-//    }
-//
-//    const fileObjects = files.map(file => {
-//      const bytes = fs.readFileSync(file)
-//      const data = new Uint8Array(bytes)
-//      return { path: path.join(path.basename(filePath), path.relative(filePath, file)), content: data }
-//    })
-//
-//    const node = onchfs.files.prepare(fileObjects)
-//    const dirCIDHex = uint8ArrayToHex(node.cid)
-//    console.log('Directory CID:', dirCIDHex);
-//
-//    const inscriptions = await onchfs.inscriptions.prepare(node)
-//    const batches = onchfs.inscriptions.batch(inscriptions, 32000)
-//
-//    for (const batch of batches) {
-//      await writeInscriptions(Tezos, batch)
-//    }
-//
-//    console.log('Directory upload completed successfully.');
-//  } else {
-//    console.error('Error: Path is neither a file nor a directory.');
-//    process.exit(1);
-//  }
+  } else if (stats.isDirectory()) {
+    const files = getAllFilesSync(filePath)
+
+    const fileObjects = files.map(file => {
+      const bytes = fs.readFileSync(file)
+      const data = new Uint8Array(bytes)
+      return { path: path.join(path.basename(filePath), path.relative(filePath, file)), content: data }
+    })
+
+    const node = onchfs.files.prepare(fileObjects)
+    const dirCIDHex = uint8ArrayToHex(node.cid)
+    console.log('Directory CID:', dirCIDHex);
+
+    const inscriptions = await onchfs.inscriptions.prepare(node)
+    const batches = onchfs.inscriptions.batch(inscriptions, CONFIG.network.BATCH_SIZE_LIMIT)
+    const tezbatches = await buildBatches(Tezos, batches)
+    const batchcost = await calcBatchCost(Tezos, tezbatches[0])
+
+    console.log(`A total of ${tezbatches.length} batch operations is required.`)
+    await confirmCost(batchcost * tezbatches.length)
+    await sendBatches(Tezos, tezbatches)
+
+    console.log('Directory upload completed successfully.');
+  } else {
+    console.error('Error: Path is neither a file nor a directory.');
+    process.exit(1);
+  }
 }   
